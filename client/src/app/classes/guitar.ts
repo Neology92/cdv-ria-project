d01b47be276bf64fcdef01985dfa56a525275d27
Oskar Legner <oskarlegner@gmail.com>
2022-01-31 16:15:25 +0100

    Update guitar model

--- client/src/app/classes/guitar.ts
+++ client/src/app/classes/guitar.ts
@@ -2,10 +2,12 @@ export class Guitar {
   id?: number;
   type: string;
   brand: string;
+  imgUrl: string;
   prodYear: number;
-  constructor(type: string, brand: string, prodYear: number, id?: number) {
+  constructor(type: string, brand: string, prodYear: number, imgUrl?: string, id?: number) {
     this.type = type;
     this.brand = brand;
+    this.imgUrl = imgUrl;
     this.prodYear = prodYear;
     this.id = id;
   }
